ec729c57c1f6105d532c86ea4a70055c52eb78d9
cheezer <subunc5@gmail.com>
2025-02-12 14:09:24 +0000

    upload thumbnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"log"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +35,78 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	videoMetaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find Video", err)
+		return
+	}
+	if videoMetaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+		return
+	}
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	maxMemory := 10 << 20
+	r.ParseMultipartForm(int64(maxMemory))
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "No file in request", err)
+		return
+	}
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse media type", err)
+		return
+	}
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusUnsupportedMediaType, "Please upload a .png or .jpeg", err)
+		return
+	}
+
+	fileExtension, err := mime.ExtensionsByType(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get file extension", err)
+		return
+	}
+	if len(fileExtension) == 0 {
+		respondWithError(w, http.StatusBadRequest, "No known file extension for the provided Content-Type", err)
+		return
+	}
+	log.Println(fileExtension)
+
+	randBytes := make([]byte, 32)
+	_, err = rand.Read(randBytes)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't generate random string", err)
+	}
+
+	encoded := base64.RawURLEncoding.EncodeToString(randBytes)
+
+	filePath := filepath.Join(cfg.assetsRoot, encoded+fileExtension[0])
+	log.Println(filePath)
+
+	assetFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create file", err)
+		return
+	}
+	defer assetFile.Close()
+	_, err = io.Copy(assetFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't copy file to "+filePath, err)
+		return
+	}
+
+	dataUrl := fmt.Sprintf("http://localhost:%v/%v", cfg.port, filePath)
+	videoMetaData.ThumbnailURL = &dataUrl
+
+	if err := cfg.db.UpdateVideo(videoMetaData); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't Update Thumbnail", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMetaData)
 }
